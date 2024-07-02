2d7ec28135124f6c7c79ec2dec84440140bce5d5
Elvis Saavedra <elvis.saavedra@nearsure.com>
2024-07-01 22:34:12 -0500

    Todo Model

---
+++ src/main/java/org/idat/todo/model/Todo.java
@@ -0,0 +1,23 @@
+package org.idat.todo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 100, nullable = false, unique = true)
+    private String title;
+    @Column(nullable = false)
+    private int priority;
+    private boolean completed;
+}
